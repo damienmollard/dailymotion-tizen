89b46e01382a65d7789e341618d79279095c3415
Damien MOLLARD <dmollard@snapp.fr>
2013-10-21 19:30:00 +0200

    Add drop view and remove iframe

--- js/main.js
+++ js/main.js
@@ -1,84 +1,16 @@
-var urls = [];
-var logUrl = true;
-
-function goBack() {
-    if (urls.length === 0) {
-        tizen.application.getCurrentApplication().exit();
-    } else {
-        logUrl = false;
-        urls.shift();
-        console.log(urls);
-        document.getElementById('iframeDaily').contentWindow.history.back();
-    }
-}
-
 $(document).ready(function() {
 
-    // Spinner
-    var Spinner = (function(){
-        var spinner = document.getElementById('spinner');
-
-        return {
-            show: function() { spinner.style.display = 'block'; },
-            hide: function() { spinner.style.display = 'none'; }
-        };
-    }());
-
-    // Cover Page
-    var Cover = (function(){
-        var cover = document.getElementById('cover'),
-            $cover = $(cover);
-
-        return {
-        	show: function() { $cover.removeClass('off'); },
-        	hide: function() { $cover.addClass('off'); }
-        };
-    }());
-
-    // Iframe
-    var $iframeDaily = $('#iframeDaily'),
-        iframeDaily = $iframeDaily[0];
-
-    $iframeDaily.on('load', function() {
-
-        // Start App
-        setTimeout(function() {
-            Spinner.hide();
-            Cover.hide();
-        }, 0);
-
-        iframeDaily.contentWindow.addEventListener('pagechange', function(e) {
-            if (!logUrl) {
-                logUrl = true;
-                return;
-            }
-            urls.unshift(e.data);
-            console.log(urls);
-        });
-    });
+    var url = 'http://touch.preprod.dailymotion.com'; // 'http://touch.dailymotion.com';
 
     var reqAppControl = tizen.application.getCurrentApplication().getRequestedAppControl();
     if (reqAppControl && reqAppControl.appControl && reqAppControl.appControl.operation === 'video') {
-        iframeDaily.src = reqAppControl.appControl.uri;
-    } else {
-        iframeDaily.src = 'http://touch.dailymotion.com/';
+        // App was launched from DynamicBox : deep link to video
+        url = reqAppControl.appControl.uri;
     }
 
-    document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName === "back") {
-            goBack();
-        }
-    });
-
-    /*
-    // Enable screen auto-rotation
-    tizen.systeminfo.addPropertyValueChangeListener('DEVICE_ORIENTATION', function(orientation) {
-        var orientationStatus = orientation.status.toLowerCase().replace('_', '-');
-        screen.unlockOrientation();
-        screen.lockOrientation(orientationStatus);
-    }, function(error) {
-        throw "Impossible de modifier l'orientation.";
-    });
-    */
+    // Show splash screen for 3 seconds
+    setTimeout(function() {
+        location.replace(url);
+    }, 3000);
 
 });
--- js/pd.js
+++ js/pd.js
@@ -1,19 +1,87 @@
-(function () {
+var videos = [];
 
-    $('button').click(function() {
+function selectedVideoAtIndex(index) {
+    var video = videos[index];
+    $('#title').html(video.title);
+    $('#owner').html(video['owner.screenname']);
+}
 
-        /*
-        var appControl = new tizen.ApplicationControl("http://tizen.org/appcontrol/operation/view", 'http://www.google.fr');
-        tizen.application.launchAppControl(appControl, null,
-            function() {
-                console.log("launch application control succeed");
-            },
-            function(e) {
-                console.log("launch application control failed. reason: " + e.message);
-            }
-        );
-        */
-        tizen.application.launch('3vhELnN8vM.Dailymotion');
+function refresh() {
+    videos = JSON.parse(localStorage.videos);
+
+    $slidee = $('#slidee');
+    $slidee.empty();
+
+    $.each(videos, function(i, video) {
+        $thumbnail = $('<li></li>');
+        $thumbnail.css('background-image', 'url('+video.thumbnail_360_url+')');
+        $thumbnail.click(function() {
+            var appControl = new tizen.ApplicationControl('video', video.url);
+            tizen.application.launchAppControl(appControl, tizen.application.getCurrentApplication().appInfo.id,
+                function() {
+                    console.log("launch application control succeed");
+                },
+                function(e) {
+                    console.log("launch application control failed. reason: " + e.message);
+                }
+            );
+        });
+        $slidee.append($thumbnail);
     });
+
+    var sly = new Sly('#frame', {
+        horizontal: 1,
+        itemNav: 'forceCentered',
+        smart: 1,
+        activateMiddle: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 0,
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        easing: 'swing'
+    }).init();
+
+    sly.on('active', function(eventName, itemIndex) {
+        selectedVideoAtIndex(itemIndex);
+    });
+
+    selectedVideoAtIndex(0);
+
+    $('#reload').removeClass('reload-rotate');
+}
+
+$(function () {
+
+    refresh();
+
+    $('#dailymotion').click(function() {
+        tizen.application.launch(tizen.application.getCurrentApplication().appInfo.id);
+    });
+
+    $('#reload').click(function() {
+        $(this).addClass('reload-rotate');
+        window.appwidget.sendMessageToBox('reload');
+    });
+
+    if (typeof window.appwidget != 'undefined') {
+        listenBoxMessage();
+    } else {
+        window.addEventListener("appwidgetready", onAppWidgetReady, false);
+    }
+
+    function onAppWidgetReady() {
+        listenBoxMessage();
+    }
+
+    function listenBoxMessage() {
+        window.addEventListener("boxmessage",
+        function(e) {
+            refresh();
+        }, false);
+    }
 
 }());
